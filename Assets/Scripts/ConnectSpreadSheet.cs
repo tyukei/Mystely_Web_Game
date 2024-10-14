cb2dd99630d19d9fdacee921e5dc33c462fae881
tyukei <0024481294bxyk6037@gmail.com>
2024-10-14 15:03:09 +0900

    update: list string story text

--- Assets/Scripts/ConnectSpreadSheet.cs
+++ Assets/Scripts/ConnectSpreadSheet.cs
@@ -11,7 +11,7 @@ public class ConnectSpreadSheet : MonoBehaviour {
     //     ReLoadGoogleSheet(1);
     // }
 
-IEnumerator Method(string _SHEET_NAME){
+IEnumerator Method(string _SHEET_NAME, System.Action<List<string>> callback){
     UnityWebRequest request = UnityWebRequest.Get("https://docs.google.com/spreadsheets/d/"+SHEET_ID+"/gviz/tq?tqx=out:csv&sheet="+_SHEET_NAME);
     yield return request.SendWebRequest();
 
@@ -21,22 +21,21 @@ IEnumerator Method(string _SHEET_NAME){
     else{
         // CSVデータを取得して表示
         string csvData = request.downloadHandler.text;
-        Debug.Log("Raw CSV Data:\n" + csvData);
-
-        // 改行で行ごとにデータを分割
         string[] rows = csvData.Split('\n');
-        Debug.Log("Number of Rows: " + rows.Length);
+
+        List<string> rowData = new List<string>();
 
         // 各行をカンマで分割して列データを表示
         foreach (string row in rows) {
             string[] columns = row.Split(',');
             foreach (string column in columns) {
-                Debug.Log("Column Data: " + column);
+                rowData.Add(column.Trim());
             }
         }
+        callback(rowData);
     }
 }
-    public void ReLoadGoogleSheet(int sheetnum){
-        StartCoroutine(Method(SHEET_NAME[sheetnum]));     
+    public void ReLoadGoogleSheet(int sheetnum, System.Action<List<string>> callback) {
+        StartCoroutine(Method(SHEET_NAME[sheetnum],callback));     
     }   
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,16 +1,28 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+
 public class GameController : MonoBehaviour
 {
     [SerializeField] private GameView view;
     [SerializeField] private Button btnStart;
+
+    // Story
+    private List<List<string>> storyTexts = new List<List<string>>();
+    private ConnectSpreadSheet connectSpreadSheet;
+    [SerializeField] private Button btnNextStory;
+
     [SerializeField] private Button btnRestart;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
         btnStart.onClick.AddListener(GameStart);
         btnRestart.onClick.AddListener(GameRestart);
+
+        // Story
+        SetStory();
     }
 
     void GameStart(){
@@ -21,5 +33,16 @@ public class GameController : MonoBehaviour
     void GameRestart(){
         string sceneName = SceneManager.GetActiveScene().name;
         SceneManager.LoadScene(sceneName);
+    }
+
+    void SetStory(){
+        if(connectSpreadSheet == null){
+            connectSpreadSheet = GetComponent<ConnectSpreadSheet>();
+        }
+
+        List<string> texts;
+        connectSpreadSheet.ReLoadGoogleSheet(0, texts => {
+            storyTexts.Add(texts);  
+        });
     }
 }
